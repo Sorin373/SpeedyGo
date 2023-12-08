f14ca61ee36515cb964cd72ebc2fc2f43b8356d9
Tudose Sorin <sorin.andrei.tudose@gmail.com>
2023-12-08 22:39:12 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
-/*
+/**
+ *                MIT License
+ *                
+ *                Copyright (c) 2023 Sorin Tudose
+ *                
+ *                Permission is hereby granted, free of charge, to any person obtaining a copy
+ *                of this software and associated documentation files (the "Software"), to deal
+ *                in the Software without restriction, including without limitation the rights
+ *                to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *                copies of the Software, and to permit persons to whom the Software is
+ *                furnished to do so, subject to the following conditions:
+ *                
+ *                The above copyright notice and this permission notice shall be included in all
+ *                copies or substantial portions of the Software.
+ *                
+ *                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *                IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *                FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *                AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *                LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *                OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *                SOFTWARE.
+ * 
  *
  *                SpeedyGo - A Depot Management Application
  *
  * Comaptibility: I provide both UNIX and WINDOWS executables since I also wanted to learn about system compatibility.
  *                It is tested on Ubuntu 22.04 and Windows 10 (22H2).
  *
- * Author:        Sorin Tudose
+ * @author        Sorin Tudose
  *
- * Description:   This application manages depots restocking operations, including inventory analysis, database visualization, and more.
+ * @brief:        This application manages depots restocking operations, including inventory analysis, database visualization, and more.
  *                This app was created from scratch as a final project only by using some sources and documentation for the 3rd party libraries and vcpkg
  *
  *                (https://github.com/nlohmann/json)
@@ -21,6 +43,8 @@
  *                ! Please report any bugs !
  *
  *                Thank you for checking out my project! :D
+ * 
+ * @file          main.cpp
  *
  */
 
